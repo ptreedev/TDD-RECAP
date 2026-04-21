3adcd9733f4f41e75f4f086ee5bfc28f0bec443f
Peter Leaver <166836879+ptreedev@users.noreply.github.com>
2026-04-21 12:25:00 +0100

    add test and implementation return list of duties for index()

--- app.py
+++ app.py
@@ -4,8 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return ['duty1', 'duty2', 'duty3', 'duty4']
+    return [{
+        'name':'duty1', 'description': 'Description of duty1'
+        }, 
+        {   'name':'duty2', 'description': 'Description of duty2'
+        }, 
+        {   'name':'duty3', 'description': 'Description of duty3'
+        }, 
+        {   'name':'duty4', 'description': 'Description of duty4'
+        }
+        ]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,26 @@
 from app import index
 
-def test_index_returns_a_list_of_duties():
-    result = index()
-    duty_list = ['duty1', 'duty2', 'duty3', 'duty4']
-    for duty in duty_list:
-        assert duty in result
+# def test_index_returns_a_list_of_duties():
+#     result = index()
+#     duty_list = ['duty1', 'duty2', 'duty3', 'duty4']
+#     for duty in duty_list:
+#         assert duty in result
 
+def test_index_returns_a_list_of_duty_dictionaries():
+    result = index()
+    duty_dict_list = [{
+        'name': 'duty1',
+        'description': 'Description of duty1',
+    }, {
+        'name': 'duty2',
+        'description': 'Description of duty2',
+    }, {
+        'name': 'duty3',
+        'description': 'Description of duty3',
+    }, {
+        'name': 'duty4',
+        'description': 'Description of duty4',
+    }]
+    assert isinstance(result[0], dict)
+
+    assert result == duty_dict_list
